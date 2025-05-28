c36300e9adc09e8de2067e9c200fc0f8068454ef
TsareJ <tsarejoseph@gmail.com>
2025-05-28 19:49:16 -0400

    Order and Drinks class being constructed

--- src/main/java/com/ps/Drink.java
+++ src/main/java/com/ps/Drink.java
@@ -1,4 +1,25 @@
 package com.ps;
 
-public class Drink {
+public class Drink implements Product{
+    private Size size;
+    private String flavor;
+
+    public Drink(Size size, String flavor) {
+        this.size = size;
+        this.flavor = flavor;
+    }
+
+    @Override
+    public double calcPrice() {
+        return switch (size) {
+            case SMALL -> 2.00;
+            case MEDIUM -> 2.50;
+            case LARGE -> 3.00;
+        };
+    }
+
+    @Override
+    public String toString() {
+        return size + " " + flavor
+    }
 }
--- src/main/java/com/ps/Order.java
+++ src/main/java/com/ps/Order.java
@@ -1,4 +1,26 @@
 package com.ps;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order {
+    private List<Product> products = new ArrayList<>();
+    public LocalDateTime orderTime = LocalDateTime.now();
+    public void addProduct (Product p) {
+        products.add(p);
+    }
+    public double calcTotal() {
+        return products.stream().mapToDouble(Product ::calcPrice).sum();
+    }
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder("~~~~~ Order Summary ~~~~~\n");
+        for (Product p: products) {
+            stringBuilder.append(p.toString()).append("\n\n");
+            stringBuilder.append(String.format("Total: $%.2f\n", calcTotal()));
+
+        }
+        return stringBuilder.toString();
+    }
 }
